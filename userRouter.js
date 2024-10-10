4166e5f14ecda393c69ea7ba477a794397fde717
Shubham222211 <123923464+Shubham222211@users.noreply.github.com>
2024-10-10 12:51:20 +0530

    Replaced bcrypt with bcryptjs and reinstalled dependencies

--- userRouter.js
+++ userRouter.js
@@ -2,7 +2,7 @@ const express=require('express')
 const jwt=require('jsonwebtoken')
 const userModel = require('./userModel')
 const userRouter=express.Router()
-const bcrypt=require('bcrypt')
+const bcrypt=require('bcryptjs')
 
 userRouter.post('/register',async(req,res)=>{
 
